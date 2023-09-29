e294ab155d066162685004ae353680661bb8eb80
Joseph l Wipperfurth <35477479+kashi059@users.noreply.github.com>
2023-09-29 09:53:03 -0500

    added favorites

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,20 +1,16 @@
-
-
 import React from 'react'
 
 export default function Favorites() {
+
+
   return (
-    <div>Favorites</div>
+  <>
     <h1>Displaying All Favorites</h1>
     <button> </button>
     <button> </button>
+</>
   )
 }
 
 
 
-return (
-    <>
-
-    </>
-)
